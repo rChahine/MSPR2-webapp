613f1ddf866298fac66df226356370ac10ebf081
AmongTheDevil <AmongTheDevil@users.noreply.github.com>
2021-02-22 16:59:20 +0100

    front login page

--- webAppMSPR2/src/components/Login.vue
+++ webAppMSPR2/src/components/Login.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="login">
-    <div class="header">
-
-    </div>
+    <img src="../assets/LogoMSPR2.png">
     <div class="body-content">
-
+      <form id="loginForm" action="/" method="post"><!-- action = URL où doit être envoyé le formulaire -->
+        <div id="emailLogin">
+          <label for="mail">E-mail</label>
+          <br />
+          <input type="email" id="mail" name="user_mail">
+        </div>
+        <div>
+          <label for="psw">Password</label>
+          <br />
+          <input id="psw" type="password" name="user_psw">
+        </div>
+        <button id="connectButton">Se connecter</button>
+      </form>
     </div>
-    <div class="footer">
-
-    </div>
-  </div>  
+  </div>
 </template>
 
 <script>
