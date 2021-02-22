58991903615b043d69aadf7e9131e9f76e9db17f
AmongTheDevil <AmongTheDevil@users.noreply.github.com>
2021-02-22 13:27:37 +0100

    initialisation interface

---
+++ webAppMSPR2/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="login">
+    <div class="header">
+
+    </div>
+    <div class="body-content">
+
+    </div>
+    <div class="footer">
+
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
