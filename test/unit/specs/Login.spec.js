ba3d541396ec009dd1813e6ee76cffa8f77f6088
Romain Chahine <32666009+rChahine@users.noreply.github.com>
2021-03-10 15:44:44 +0100

    Update and rename HelloWorld.spec.js to Login.spec.js

---
+++ test/unit/specs/Login.spec.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Login from '@/components/Login'
+
+describe('Login.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(Login)
+    const vm = new Constructor().$mount()
+    expect(true).toBeTrue();
+  })
+})
